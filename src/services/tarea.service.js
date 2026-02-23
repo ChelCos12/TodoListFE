76544b11d6a995d17e31f84f194bd4b2d635a3be
ChelCos12 <marcelo.cosme.pamuri@gmail.com>
2026-02-23 11:07:55 -0400

    BT-4 creacion del archivo tarea.service.js y sus funciones

---
+++ src/services/tarea.service.js
@@ -0,0 +1,24 @@
+const API_BASE_URL = 'http://127.0.0.1:8000/api';
+
+const tareaService = {
+
+  getAll: async () => {
+  },
+
+  create: async (tarea) => {
+  },
+
+  update: async (id, tarea) => {
+  },
+
+  delete: async (id) => {
+  },
+
+  getOne: async (id) => {
+  },
+
+  toggle: async (id) => {
+  }
+};
+
+export default tareaService;
